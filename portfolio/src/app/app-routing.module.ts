d140c238e81aad8ffd20d7ac4a37f48086e020ec
Rahul-webinsane <webinsanestudios@gmail.com>
2023-09-20 23:17:09 +0530

    new cv updated

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: " ", component: HomeComponent, pathMatch: 'full'},
+  {path: "home", component: HomeComponent},
+  // {path: "services", component: ServicesComponent},
+  {path: "about", component: AboutMeComponent},
+  {path: "contact", component: ContactUsComponent},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
